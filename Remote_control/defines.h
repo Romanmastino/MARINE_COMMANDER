a5d47b8a12ac8d3615794e4204d130e14c2f991f
Romanmastino <63642046+Romanmastino@users.noreply.github.com>
2023-08-03 21:57:10 +0200

    Create defines.h

---
+++ Remote_control/defines.h
@@ -0,0 +1,34 @@
+#ifndef defines.h
+#define defines.h
+
+//INPUTS
+        
+    #define UP 1  //BOWTHRUSTER LEFT / R49
+    #define DOWN 2  //BOWTHRUSTER RIGHT / R48
+    #define EAST 3 //LEFT ENGINE FORWARD / R47
+    #define WEST 4 //RIGHT ENGINE FORWARD / R46
+    #define SOUTH 5 //LEFT ENGINE BACKWARD / R45
+    #define NORTH 6 //RIGHT ENGINE BACKWARD / R44
+    #define R1 7 //STERNTHRUSTER LEFT / R43
+    #define R2 8 //STERNTHRUSTER RIGHT / R42
+    #define R0 9 //HORN / R41
+
+
+//OUTPUTS
+
+    #define BOWTHRUSTER_LEFT_PIN
+    #define BOWTHRUSTER_RIGHT_PIN
+    
+    #define LEFT_ENGINE_FORWARD_PIN
+    #define LEFT_ENGINE_BACKWARD_PIN
+    #define LEFT_ENGINE_NEUTRAL_SWITCH_PIN
+    
+    #define RIGHT_ENGINE_FORWARD_PIN    
+    #define RIGHT_ENGINE_BACKWARD_PIN
+    #define RIGHT_ENGINE_NEUTRAL_SWITCH_PIN
+    
+    #define STERNTHRUSTER_LEFT_PIN
+    #define STERNTHRUSTER_RIGHT_PIN
+    
+    #define HORN_PIN
+    
