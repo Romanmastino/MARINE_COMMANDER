c6253d66b0f19bdcbd276dd89d23de71250bef2b
Romanmastino <63642046+Romanmastino@users.noreply.github.com>
2023-10-05 18:50:14 +0300

    V2

--- Remote_control/Remote_control.ino
+++ Remote_control/Remote_control.ino
@@ -15,7 +15,6 @@ unsigned long debug_timer;
 void setup() 
 
 {
-
     pinMode(UP_PIN, INPUT);
     pinMode(DOWN_PIN, INPUT);
     pinMode(EAST_PIN, INPUT);
@@ -38,33 +37,111 @@ void setup()
     pinMode(STERNTHRUSTER_LEFT_PIN, OUTPUT);
     pinMode(STERNTHRUSTER_RIGHT_PIN, OUTPUT);
     pinMode(HORN_PIN, OUTPUT);
-    pinMode(HORN_PIN, OUTPUT);
+    pinMode(BUZZER, OUTPUT);
+    pinMode(AUX_1, OUTPUT);
+    
+    
+    digitalWrite(LEFT_ENGINE_FORWARD_PIN, LOW);
+    digitalWrite(LEFT_ENGINE_REVERSE_PIN, LOW);
+    digitalWrite(RIGHT_ENGINE_FORWARD_PIN, LOW);
+    digitalWrite(RIGHT_ENGINE_REVERSE_PIN, LOW);
+
+    
+    digitalWrite(BUZZER, HIGH);
+    delay(1000);
+    digitalWrite(BUZZER, LOW);
+    Serial.begin(115200);
+    Serial.println("PILOT-H STARTED");
+    
+
 
 }
 
 void loop() {
 readInputs();
-debug();
+//debug();
+controlOutputs();
 }
 
 void readInputs()
 {
- BOWTHRLEFT = !digitalRead(UP_PIN);
- BOWTHRRIGHT = !digitalRead(DOWN_PIN);
- LEFTENGFWD = !digitalRead(EAST_PIN);
- RIGHTENGFWD = !digitalRead(WEST_PIN);
- LEFTENGREV = !digitalRead(SOUTH_PIN);
- RIGHTENGREV = !digitalRead(NORTH_PIN);
- STERNTHRLEFT = !digitalRead(R1_PIN);
- STERNTHRRIGHT = !digitalRead(R2_PIN);
- HORN = !digitalRead(R0_PIN);
+ BOWTHRLEFT = digitalRead(UP_PIN);
+ BOWTHRRIGHT = digitalRead(DOWN_PIN);
+ LEFTENGFWD = digitalRead(EAST_PIN);
+ RIGHTENGFWD = digitalRead(WEST_PIN);
+ LEFTENGREV = digitalRead(SOUTH_PIN);
+ RIGHTENGREV = digitalRead(NORTH_PIN);
+ STERNTHRLEFT = digitalRead(R1_PIN);
+ STERNTHRRIGHT = digitalRead(R2_PIN);
+ HORN = digitalRead(R0_PIN);
 }
+
+
+void controlOutputs()
+{
+  
+    if(BOWTHRLEFT == 0)
+    digitalWrite(BOWTHRUSTER_LEFT_PIN, HIGH);
+    else
+    digitalWrite(BOWTHRUSTER_LEFT_PIN, LOW);
+
+    if(BOWTHRRIGHT == 0)
+    digitalWrite(BOWTHRUSTER_RIGHT_PIN, HIGH);
+    else
+    digitalWrite(BOWTHRUSTER_RIGHT_PIN, LOW);
+
+    if(LEFTENGFWD == 0)
+    digitalWrite(LEFT_ENGINE_FORWARD_PIN, HIGH);
+    else
+    digitalWrite(LEFT_ENGINE_FORWARD_PIN, LOW);
+
+    if(LEFTENGREV == 0)
+    digitalWrite(LEFT_ENGINE_REVERSE_PIN, HIGH);
+    else
+    digitalWrite(LEFT_ENGINE_REVERSE_PIN, LOW);
+
+    if(LEFTENGFWD == 0 || LEFTENGREV == 0)
+    digitalWrite(LEFT_ENGINE_NEUTRAL_SWITCH_PIN, HIGH);
+    else
+    digitalWrite(LEFT_ENGINE_NEUTRAL_SWITCH_PIN, LOW);
+
+    if(RIGHTENGFWD == 0)
+    digitalWrite(RIGHT_ENGINE_FORWARD_PIN, HIGH);
+    else
+    digitalWrite(RIGHT_ENGINE_FORWARD_PIN, LOW);
+    
+    if(RIGHTENGREV == 0)
+    digitalWrite(RIGHT_ENGINE_REVERSE_PIN, HIGH);
+    else
+    digitalWrite(RIGHT_ENGINE_REVERSE_PIN, LOW);
+
+    if(RIGHTENGFWD == 0 || RIGHTENGREV == 0)
+    digitalWrite(RIGHT_ENGINE_NEUTRAL_SWITCH_PIN, HIGH);
+    else
+    digitalWrite(RIGHT_ENGINE_NEUTRAL_SWITCH_PIN, LOW);
+
+    if(STERNTHRLEFT == 0)
+    digitalWrite(STERNTHRUSTER_LEFT_PIN, HIGH);
+    else
+    digitalWrite(STERNTHRUSTER_LEFT_PIN, LOW);
+
+    if(STERNTHRRIGHT == 0)
+    digitalWrite(STERNTHRUSTER_RIGHT_PIN, HIGH);
+    else
+    digitalWrite(STERNTHRUSTER_RIGHT_PIN, LOW);
+ 
+    if(HORN == 0)
+    digitalWrite(HORN_PIN, HIGH);
+    else
+    digitalWrite(HORN_PIN, LOW);
+    
+}
+
 
 void debug()
 {
   if(debug_timer <= millis())
   {
-
   
 if(BOWTHRLEFT == 0)
 Serial.println("BOW THRUSTER LEFT");
@@ -80,12 +157,10 @@ if(RIGHTENGREV == 0)
 Serial.println("RIGHT ENGINE REVERSE");
 if(STERNTHRLEFT == 0)
 Serial.println("STERN THRUSTER LEFT");
-if(STERNTHRLEFT == 0)
+if(STERNTHRRIGHT == 0)
 Serial.println("STERN THRUSTER RIGHT");
 if(HORN == 0)
 Serial.println("HORN");
 debug_timer = millis() + 100;
 }
 }
-
-    
